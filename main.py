2903762623332f47cb39386aa5a555f32fc25ee9
Yang Si Jun <aersijun@gmail.com>
2024-01-16 02:49:40 +0800

    set layout

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import logging
-from functools import wraps
-
 import telegram
+import firebase_admin
+
+from functools import wraps
 from telegram import Update, ReplyKeyboardRemove, ChatAction, ReplyKeyboardMarkup
 from telegram.ext import CallbackContext, ConversationHandler, Updater, CommandHandler, MessageHandler, Filters
-import firebase_admin
-from firebase_admin import db
+import posters
 
 PORT = int(os.environ.get('PORT', '8443'))
 
@@ -18,20 +18,27 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 """initialising variables/bot"""
-TOKEN = ''
+TOKEN = '6397262167:AAF5JUnqD_spZzNeGPWmf-nDX_X2VeUgbiQ'
 bot = telegram.Bot(token=TOKEN)
 luckydraw_no = 1
-voting_dict = {'1': "placeholder1", '2': "placeholder2", '3': "placeholder3", '4':"placeholder4"}
 
 # define no. of variables to be stored
-VOTING, REVOTING, REVOTED = range(3)
+VOTING, CATEGORY, UROP, OVERSEAS, FIFTHROW = range(5)
+
+# Flow 1
+""" start --> category --> urop --> voting_urop """
+# Flow 2
+"""Overseas has an additional choosing of country before the final poster selection"""
+""" start --> category --> overseas --> overseas_phase2 --> voting_overseas"""
+# Flow 3
+""" start --> category --> fifthrow --> voting_fifthrow"""
 
 """Initialising firebase creds"""
 cred_obj = firebase_admin.credentials.Certificate('./creds.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
-    'databaseURL': ''
+    'databaseURL': 'https://lcc-luckydraw-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
-ref = db.reference("/")
+ref = firebase_admin.db.reference("/")
 
 
 def send_typing_action(func):
@@ -49,20 +56,60 @@ def send_typing_action(func):
 def start(update: Update, _: CallbackContext):
     """Starts the conversation."""
     user = update.message.from_user
-    reply_keyboard = [['1', '2', '3', '4']]
+    reply_keyboard = [['1', '2', '3']]
     update.message.reply_text(
-        "Thank you for participating in LCC Voting. \n\nYou are only allowed to vote for one poster. You will "
-        "automatically be entered into a lucky draw when you vote for your "
+        "Thank you for participating in LCC Voting. You can view all the posters on"
+        "our website https://lcc.sutd.edu.sg/. \n\nYou are only allowed to vote for one poster. You will "
+        "automatically be entered into a lucky draw when you vote for your"
         "favourite poster.\n\n"
-        "Which poster would you like to vote for? \n\n"
-        "1: Poster 1 \n"
-        "2: Poster 2 \n"
-        "3: Poster 3 \n"
-        "4: Poster 4 \n\n"
+        "Which category would you like to vote for \n\n"
+        "1: UROP \n"
+        "2: Overseas Opportunities \n"
+        "3: Fifth Rows \n\n"
         "Send /cancel to stop talking to me.", reply_markup=ReplyKeyboardMarkup(reply_keyboard)
     )
-    logger.info("User %s is voting", user.first_name)
-    return VOTING
+    logger.info("User %s is choosing a category", user.first_name)
+    return CATEGORY
+
+@send_typing_action
+def category(update: Update, _: CallbackContext):
+    """Let user choose the options based on category"""
+    user = update.message.from_user
+    # UROP
+    if update.message.text == '1':
+        reply_keyboard = [['1', '2', '3', '4']]
+        poster_string = ''
+        for poster in posters.urop:
+            poster_string += f'{poster} \n'
+        update.message.reply_text(
+            "You have chosen the UROP category. Which poster do you wish to vote for? \n\n"
+            f'{poster_string}', reply_markup=ReplyKeyboardMarkup(reply_keyboard))
+        logger.info("User %s is choosing an option for UROP", user.first_name)
+        return UROP
+    if update.message.text == '2':
+        reply_keyboard = [['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']]
+        poster_string = ''
+        for poster in posters.overseas:
+            poster_string += f'{poster}\n'
+        update.message.reply_text(
+            "You have chosen the Overseas Opportunities category. Which University would you like to vote for? \n\n"
+            f'{poster_string}', reply_markup=ReplyKeyboardMarkup(reply_keyboard))
+        logger.info("User %s is choosing an option for Overseas Opportunities", user.first_name)
+        return OVERSEAS
+    if update.message.text == '3':
+        reply_keyboard = [['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']]
+        poster_string = ''
+        for poster in posters.fifth_rows:
+            poster_string += f'{poster}\n'
+        update.message.reply_text(
+            "You have chosen the Fifth Row category. Which Poster would you like to vote for? \n\n"
+            f'{poster_string}', reply_markup=ReplyKeyboardMarkup(reply_keyboard))
+        logger.info("User %s is choosing an option for Fifth Row", user.first_name)
+        return FIFTHROWS
+
+@send_typing_action
+def category(update: Update, _: CallbackContext):
+    """Let user choose the options based on category"""
 
 
 @send_typing_action
@@ -70,7 +117,6 @@ def voting(update: Update, _: CallbackContext):
     userID = str(update.message.chat_id)
     global luckydraw_no
     user = update.message.from_user
-    # TODO - check if userID in keys of database
     all_keys = set().union(*(d.keys() for d in ref.child("voting").get().values() if type(d) == dict))
     if userID in all_keys:
         logger.info("User %s tried to vote multiple times", user.first_name)
@@ -80,7 +126,7 @@ def voting(update: Update, _: CallbackContext):
         )
         return ConversationHandler.END
     else:
-        ref.child("voting").child(f"poster {update.message.text}").update({f"{userID}": f"{str(luckydraw_no)}"})
+        ref.child("voting").child(f"{update.message.text}").update({f"{userID}": f"{str(luckydraw_no)}"})
         update.message.reply_text(
             f"Your vote was {voting_dict[update.message.text]}.\n"
             f"Your lucky draw number is {'0' * (4 - len(str(luckydraw_no))) + str(luckydraw_no)}. \n\n"
@@ -136,7 +182,6 @@ def revoting(update: Update, _: CallbackContext):
 @send_typing_action
 def cancel(update: Update, _: CallbackContext):
     """Cancels and ends the conversation."""
-    userID = str(update.message.chat_id)
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
@@ -153,6 +198,7 @@ if __name__ == '__main__':
     start_conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
+            CATEGORY: [MessageHandler(Filters.regex('^[1-13]$'), category)],
             VOTING: [MessageHandler(Filters.regex('^[1-4]$'), voting)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
---
+++ posters.py
@@ -0,0 +1,88 @@
+# UROP
+
+urop = {'1': "Redesigning Urban Green For Health & Well-being",
+        "2": "Forecasting Air Cargo Demand For Asia",
+        "3": "Classification Of Architectural Design",
+        "4": "Walkable Southeast Asia: A Repository Of Urban Liveability Features"}
+
+# Overseas
+overseas = {}
+
+# Overseas
+fact = {
+        '1': "FACT - (DTIxUPM)",
+        '2': "FACT - (SSWxUBD)",
+        '3': "FACT - (DESxSZTU)",
+}
+
+berkley = {
+        '1': "Berkeley Summer Sessions Group 1",
+        '2': "Berkeley Summer Sessions Group 2",
+        '3': "Berkeley Summer Sessions Group 3",
+        '4': "Berkeley Summer Sessions Group 4",
+        '5': "Berkeley Summer Sessions Group 5",
+}
+
+stanford = {
+    '1': "Stanford IHP Poster Group 1",
+    '2': "Stanford IHP Poster Group 2",
+    '3': "Stanford IHP Poster Group 3",
+    '4': "Stanford Summer Session Poster",
+}
+
+berlin = {
+    '1': "TU Berlin Summer School Poster Group 1",
+    '2': "TU Berlin Summer School Poster Group 2",
+    '3': "SP TU Berlin Summer Poster",
+}
+
+aalto = {
+    '4': "Aalto Arts Poster Group",
+    '5': "Aalto ITP poster group",
+}
+
+kaist = {
+    '1': "KAIST Poster Summer Group",
+}
+
+hanyang = {
+    '1': "Hanyang Summer School Poster group"
+}
+
+yonsei = {
+    '1': "Yonsei Poster Group",
+}
+
+skku = {
+    '1': "SKKU Summer poster",
+}
+
+ubc = {
+    '1': "UBC Summer Poster Group",
+}
+
+macau = {
+    '1': "University of Macau Summer Camp",
+}
+
+iacocca = {
+    '1': "Lehigh IACOCCA INSTITUTE GLOBAL VILLAGE Summer Poster"
+}
+
+mitacs = {
+    '1': "Mitacs poster",
+}
+
+fifth_rows = {
+        '1': "Boxing Club",
+        '2': "Funktion Club",
+        '3': "Tchoukball Club",
+        '4': "SOAR Club",
+        '5': "Rotaract Club",
+        '6': "Archery Club",
+        '7': "Cuesports Club",
+        '8': "SUTD-Seds Club",
+        '9': "Chinese Orchestra Club",
+        '10': "DDZ",
+        '11': "Swimming Club",
+}
